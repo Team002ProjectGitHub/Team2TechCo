3ae456ef5d62af644d670bcc15d482c9c0b5b526
AiyasOmoGitHub <166263603+AiyasOmoGitHub@users.noreply.github.com>
2024-06-05 00:31:16 +0100

    commit

--- obj/Debug/net8.0/Team2TechCo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Team2TechCo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team2TechCo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b518644b1db3ad39eb90d0ef96eacf014abe04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33baf6aa806121b5ed2d585aad2316dff653b85b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team2TechCo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team2TechCo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
